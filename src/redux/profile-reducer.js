e6c1b932b9b14d6990ea5e0a1a891e2d6eeb494c
Anastasya <AnastasyaShapovalova>
2020-09-18 20:03:30 +0300

    local state and componentDidUpdate

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,9 +1,11 @@
-import {usersAPI} from "../api/api";
+import {profileAPI, usersAPI} from "../api/api";
 
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const DELETE_POST = 'DELETE_POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
+const SET_STATUS = 'SET_STATUS';
+
 let initialState = {
     posts: [
         {id: 1, message: 'Hi, how are you?', likesCount: 12},
@@ -12,7 +14,8 @@ let initialState = {
         {id: 4, message: 'Dada', likesCount: 12},
     ],
     newPostText: 'it-kamasutra.com',
-    profile: null
+    profile: null,
+    status: ''
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -33,6 +36,12 @@ const profileReducer = (state = initialState, action) => {
            return {
                 ...state,
                 newPostText: action.newText
+            };
+        }
+        case SET_STATUS: {
+            return {
+                ...state,
+                status: action.status
             };
         }
         case DELETE_POST: {
@@ -56,10 +65,28 @@ export const  addPostActionCreator = () => ({type: ADD_POST})
 export const  deletePostActionCreator = (id) => ({type: DELETE_POST, id})
 export const  updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText:text})
 export const  setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
+export const  setStatus = (status) => ({type: SET_STATUS, status})
+
 export const  getUserProfile = (userId) => (dispatch)  => {
     usersAPI.getProfile(userId)
         .then(response => {
           dispatch(setUserProfile(response.data));
+        });
+}
+
+export const  getStatus = (userId) => (dispatch)  => {
+    profileAPI.getStatus(userId)
+        .then(response => {
+            dispatch(setStatus(response.data));
+        });
+}
+
+export const  updateStatus = (status) => (dispatch)  => {
+    profileAPI.updateStatus(status)
+        .then(response => {
+            if (response.data.resultCode === 0) {
+                dispatch(setStatus(status));
+            }
         });
 }
 export default profileReducer;
